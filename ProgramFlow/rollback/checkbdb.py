ee617da72f07b940666928e88c0995f15e30e13b
kumarvgit <vaibhav.cs.kumar@gmail.com>
2021-11-20 12:02:56 +0000

    Update the code for using date time function

--- ProgramFlow/rollback/checkbdb.py
+++ ProgramFlow/rollback/checkbdb.py
@@ -18,8 +18,10 @@ import sqlite3
 
 db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
 
+#  This converts only from till 3 precision
 for row in db.execute("SELECT strftime('%Y-%m-%d %H:%M:%f', history.time, 'localtime') AS localtime,"
                       " history.account, history.amount FROM history ORDER BY history.time"):
+# Use python for 6 place accuracy for miliseconds
 # for row in db.execute("SELECT * FROM localhistory"):
     print(row)
 
--- ProgramFlow/rollback/rollback.py
+++ ProgramFlow/rollback/rollback.py
@@ -16,7 +16,8 @@ class Account(object):
     @staticmethod
     def _current_time():
         return pytz.utc.localize(datetime.datetime.utcnow())
-        # return 1
+        # local_time = pytz.utc.localize(datetime.datetime.utcnow())
+        # return local_time.astimezone()
 
     def __init__(self, name: str, opening_balance: int = 0):
         cursor = db.execute("SELECT name, balance FROM accounts WHERE (name = ?)", (name,))
@@ -36,15 +37,10 @@ class Account(object):
     def _save_update(self, amount):
         new_balance = self._balance + amount
         deposit_time = Account._current_time()
-
-        try:
-            db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)", (new_balance, self.name))
-            db.execute("INSERT INTO history VALUES(?, ?, ?)", (deposit_time, self.name, amount))
-        except sqlite3.Error:
-            db.rollback()
-        else:
-            db.commit()
-            self._balance = new_balance
+        db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)", (new_balance, self.name))
+        db.execute("INSERT INTO history VALUES(?, ?, ?)", (deposit_time, self.name, amount))
+        db.commit()
+        self._balance = new_balance
 
     def deposit(self, amount: int) -> float:
         if amount > 0.0:
@@ -75,7 +71,6 @@ class Account(object):
 
     def show_balance(self):
         print("Balance on account {} is {:.2f}".format(self.name, self._balance / 100))
-
 
 if __name__ == '__main__':
     john = Account("John")

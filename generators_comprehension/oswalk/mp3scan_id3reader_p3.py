46bdec3fae284d0a31c378cf478732b4fb22cbe1
kumarvgit <vaibhav.cs.kumar@gmail.com>
2022-01-14 09:18:40 +0000

    id3 in python

---
+++ generators_comprehension/oswalk/mp3scan_id3reader_p3.py
@@ -0,0 +1,33 @@
+import os
+import fnmatch
+
+import id3reader_py3 as id3reader
+
+
+def find_music(start, extension):
+    for path, directories, files in os.walk(start):
+        for file in fnmatch.filter(files, '*.{}'.format(extension)):
+            absolute_path = os.path.abspath(path)
+            yield os.path.join(absolute_path, file)
+
+
+my_music_files = find_music("music", "emp3")
+# my_music_files = find_music("<SOME_DIR>", "mp3")
+
+emp3_error_list = []
+
+for f in my_music_files:
+
+    try:
+        id3r = id3reader.Reader(f)
+        print("Artist: {}, Album: {} Track: {}, Song: {}".format(
+            id3r.get_value('performer'),
+            id3r.get_value('album'),
+            id3r.get_value('track'),
+            id3r.get_value('title')
+        ))
+    except:
+        emp3_error_list.append(f)
+
+for er in emp3_error_list:
+    print(er)

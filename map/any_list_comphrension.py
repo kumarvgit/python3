5dba3c5f95cd73d6d566f85460900dc6e586fb75
kumarvgit <vaibhav.cs.kumar@gmail.com>
2022-01-16 11:46:44 +0000

    any with dict and list

---
+++ map/any_list_comphrension.py
@@ -0,0 +1,14 @@
+from data import people, plants_list, basic_plants_list
+
+# people = []
+
+if bool(people) and all([person[1] for person in people]):
+    print("Sending email")
+else:
+    print("User must edit the list of recipients")
+
+if any([plant.plant_type == "Grass" for plant in plants_list]):
+    print("This pack contains grass")
+else:
+    print("No grasses in this pack")
+
